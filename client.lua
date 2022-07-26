c0f9af952c7ab758c4a8663804460465c26f662c
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-26 14:23:01 -0700

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,35 @@
+RegisterCommand('liveries',function()
+    TriggerEvent('getliveries')
+end)
+
+AddEventHandler('getliveries',function()
+    local options = {}
+    local vehicle = GetVehiclePedIsIn(PlayerPedId())
+    local count = GetVehicleLiveryCount(vehicle)
+    for i = 1, count do
+		options[#options + 1] = {
+			title = "Toggle livery " .. i,
+			event = 'togglelivery',
+			args = {
+				vehicle = vehicle,
+				livery = i
+			}
+		}
+    end
+	
+	options[#options + 1] = {
+		title = '❌ Close (ESC)',
+	}
+	
+    lib.registerContext({
+        id = 'liveries_menu',
+        title = 'Vehicle Liveries',
+        options = options
+    })
+    lib.showContext('liveries_menu')
+end)
+
+AddEventHandler('togglelivery',function(data)
+    SetVehicleLivery(data.vehicle,data.livery)
+    TriggerEvent('getliveries')
+end)
