d0cbc6633371fb5905c5a128a793502bf3cfa701
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-26 14:22:29 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+--[[ FX Information ]]--
+fx_version   'cerulean'
+use_fxv2_oal 'yes'
+lua54        'yes'
+game         'gta5'
+
+--[[ Resource Information ]]--
+name         'jdd_liveries'
+author       'Judd#7644'
+version      '1.0.0'
+repository   'https://github.com/juddisjudd/jdd_liveries'
+description  'Simple Liveries Menu using ox_lib'
+
+author 'Judd#7644'
+description 'Vehicle Liveries'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+}
