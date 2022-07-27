09e8b470bf93166688fc09babde7d8a7d99d5b0b
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-26 22:46:10 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -6,16 +6,33 @@ AddEventHandler('getliveries',function()
     local options = {}
     local vehicle = GetVehiclePedIsIn(PlayerPedId())
     local count = GetVehicleLiveryCount(vehicle)
+	local name = GetLabelText(GetLiveryName(vehicle, i))
     for i = 1, count do
 		options[#options + 1] = {
 			title = "Toggle livery " .. i,
+			description = name,
 			event = 'togglelivery',
 			args = {
 				vehicle = vehicle,
 				livery = i
 			}
 		}
-    end
+	end
+		
+	if GetVehicleLivery(vehicle) == -1 then
+		lib.notify({
+			id = 'some_identifier',
+			title = 'Liveries',
+			description = 'No liveries found for this vehicle.',
+			position = 'top',
+			style = {
+				backgroundColor = '#141517',
+				color = '#909296'
+			},
+			icon = 'ban',
+			iconColor = '#C53030'
+		})
+	return end
 	
     lib.registerContext({
         id = 'liveries_menu',
