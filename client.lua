52d6976cf2a6f8fe1eba30980172a98b2c96e583
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-26 22:51:31 -0700

    OCD, livery names not working atm

--- client.lua
+++ client.lua
@@ -6,40 +6,42 @@ AddEventHandler('getliveries',function()
     local options = {}
     local vehicle = GetVehiclePedIsIn(PlayerPedId())
     local count = GetVehicleLiveryCount(vehicle)
-	local name = GetLabelText(GetLiveryName(vehicle, i))
+    local name = GetLabelText(GetLiveryName(vehicle, i))
     for i = 1, count do
-		options[#options + 1] = {
-			title = "Toggle livery " .. i,
-			description = name,
-			event = 'togglelivery',
-			args = {
-				vehicle = vehicle,
-				livery = i
-			}
+	options[#options + 1] = {
+		title = "Toggle livery " .. i,
+		description = name,
+		event = 'togglelivery',
+		args = {
+			vehicle = vehicle,
+			livery = i
 		}
-	end
-		
-	if GetVehicleLivery(vehicle) == -1 then
-		lib.notify({
-			id = 'some_identifier',
-			title = 'Liveries',
-			description = 'No liveries found for this vehicle.',
-			position = 'top',
-			style = {
-				backgroundColor = '#141517',
-				color = '#909296'
-			},
-			icon = 'ban',
-			iconColor = '#C53030'
-		})
-	return end
+	}
+    end
+
+    if GetVehicleLivery(vehicle) == -1 then
+	lib.notify({
+		id = 'some_identifier',
+		title = 'Liveries',
+		description = 'No liveries found for this vehicle.',
+		position = 'top',
+		style = {
+			backgroundColor = '#141517',
+			color = '#909296'
+		},
+		icon = 'ban',
+		iconColor = '#C53030'
+	})
+    return end
 	
     lib.registerContext({
         id = 'liveries_menu',
         title = 'Vehicle Liveries',
         options = options
     })
+		
     lib.showContext('liveries_menu')
+		
 end)
 
 AddEventHandler('togglelivery',function(data)
