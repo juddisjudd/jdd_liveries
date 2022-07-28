64432c761e6fedb70fd14b1f7938350b45a154bb
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-27 23:25:43 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 --[[ FX Information ]]--
 fx_version   'cerulean'
-use_fxv2_oal 'yes'
+use_experimental_fxv2_oal 'yes'
 lua54        'yes'
 game         'gta5'
 
@@ -10,10 +10,6 @@ author       'Judd#7644'
 version      '1.0.0'
 repository   'https://github.com/juddisjudd/jdd_liveries'
 description  'Simple Liveries Menu using ox_lib'
-
-author 'Judd#7644'
-description 'Vehicle Liveries'
-version '1.0.0'
 
 client_scripts {
     'client.lua',
