6b7671783f541b48741d568e1517a55414efdd80
Skryptific <juweyrhf@gmail.com>
2022-07-30 11:14:19 +0200

    Refactor options and utilise cache vehicle

--- client.lua
+++ client.lua
@@ -1,14 +1,14 @@
+
+
 local function viewLiveries()
-	if cache.vehicle then return TriggerEvent('getliveries')
-else
+	if cache.vehicle then 
+		return TriggerEvent('getLiveries')
+	else
 		lib.notify({
 			title = 'Liveries',
 			description = 'You are not in a vehicle.',
 			position = 'top',
-			style = {
-				backgroundColor = '#141517',
-				color = '#909296'
-			},
+			style = { backgroundColor = '#141517', color = '#909296' },
 			icon = 'ban',
 			iconColor = '#C53030'
 		})
@@ -17,54 +17,61 @@ end
 
 RegisterCommand('liveries', viewLiveries)
 
-AddEventHandler('getliveries',function()
-	local vehicle = GetVehiclePedIsIn(PlayerPedId())
-	local numMods = GetNumVehicleMods(cache.vehicle, 48)
-	local numLiveries = GetVehicleLiveryCount(cache.vehicle)
+local function formatOption(label, vehicle, i)
+	return {
+		title = ('Toggle Livery: %s'):format(i + 1),
+		description = label,
+		event = 'toggleLivery',
+		args = { vehicle = vehicle, livery = i }
+	}
+end
 
-	SetVehicleModKit(cache.vehicle, 0)
+AddEventHandler('getLiveries', function()
+	local vehicle = cache.vehicle
+	local numMods = GetNumVehicleMods(vehicle, 48)
+	local numLiveries = GetVehicleLiveryCount(vehicle)
 
-	local options = {}
+	SetVehicleModKit(vehicle, 0)
+
+	local options, count = {}, 0
+
 	if numMods > 0 then
 		for i = 0, numMods -1 do
-			local modLabel = GetModTextLabel(cache.vehicle, 48, i)
-			options[#options + 1] = {
-				title = "Toggle livery " .. i,
-				description = GetLabelText(modLabel),
-				event = 'togglelivery',
-				args = {
-					vehicle = vehicle,
-					livery = i
-				}
-			}
+			count += 1
+			options[count] = formatOption(GetLabelText(GetModTextLabel(vehicle, 48, i)), vehicle, i)
 		end
 	end
 
 	if numLiveries > 0 then
 		for i = 0, numLiveries -1 do
-			local modLabel = GetLiveryName(cache.vehicle, i)
-			options[#options + 1] = {
-				title = "Toggle livery " .. i,
-				description = GetLabelText(modLabel),
-				event = 'togglelivery',
-				args = {
-					vehicle = vehicle,
-					livery = i
-				}
-			}
+			count += 1
+			options[count] = formatOption(GetLabelText(GetLiveryName(vehicle, i)), vehicle, i)
 		end
 	end
 
-    lib.registerContext({
-        id = 'liveries_menu',
-        title = 'Vehicle Liveries',
-        options = options
-    })
-    lib.showContext('liveries_menu')
+	if count == 0 then
+		return lib.notify({
+			title = 'Liveries',
+			description = 'No liveries have been found for this vehicle.',
+			position = 'top',
+			style = { backgroundColor = '#141517', color = '#909296' },
+			icon = 'ban',
+			iconColor = '#C53030'
+		})
+	end
+
+	lib.registerContext({ id = 'liveries_menu', title = 'Vehicle Liveries', options = options })
+	lib.showContext('liveries_menu')
 end)
 
-AddEventHandler('togglelivery', function(data)
-	SetVehicleLivery(data.vehicle, data.livery)
-	SetVehicleMod(data.vehicle, 48, data.livery)
-	TriggerEvent('getliveries')
+AddEventHandler('toggleLivery', function(data)
+	local vehicle, livery = data?.vehicle, data?.livery
+	if not vehicle or not livery then
+		return
+	end
+
+	SetVehicleLivery(vehicle, livery)
+	SetVehicleMod(vehicle, 48, livery)
+
+	TriggerEvent('getLiveries')
 end)
