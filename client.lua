c97708f871b688a4c04a4daa5357c681749d2fd7
Judd <85649796+juddisjudd@users.noreply.github.com>
2022-07-30 00:47:18 -0700

    Get all liveries and labels (if they exist)
    We now get all liveries and labels (if they have them)
    Thanks to Linden for helping me get over the last hurdles.

--- client.lua
+++ client.lua
@@ -3,7 +3,7 @@ RegisterCommand('liveries',function()
     	TriggerEvent('getliveries')
 	else 
 		lib.notify({
-			id = 'invehicle',
+			id = 'some_identifier',
 			title = 'Liveries',
 			description = 'You are not in a vehicle.',
 			position = 'top',
@@ -18,35 +18,42 @@ RegisterCommand('liveries',function()
 end)
 
 AddEventHandler('getliveries',function()
-    local vehicle = GetVehiclePedIsIn(PlayerPedId()) -- Get the current vehicle the player is in
-    local count = GetVehicleLiveryCount(vehicle) -- Get the count of all liveries vehicle has
-    local name = GetLabelText(GetLiveryName(vehicle, liveryIndex)) -- This should be returning livery name
-    local options = {}
-    for liveries = 1, count do
-		options[#options + 1] = {
-			title = "Toggle livery " .. liveries,
-			description = name,
-			event = 'togglelivery',
-			args = {
-				vehicle = vehicle,
-				livery = liveries
+	local vehicle = GetVehiclePedIsIn(PlayerPedId())
+	local numMods = GetNumVehicleMods(cache.vehicle, 48)
+	local numLiveries = GetVehicleLiveryCount(cache.vehicle)
+	
+	SetVehicleModKit(cache.vehicle, 0)
+
+	local options = {}
+	if numMods > 0 then
+		for i = 0, numMods -1 do
+			local modLabel = GetModTextLabel(cache.vehicle, 48, i)
+			options[#options + 1] = {
+				title = "Toggle livery " .. i,
+				description = GetLabelText(modLabel),
+				event = 'togglelivery',
+				args = {
+					vehicle = vehicle,
+					livery = i
+				}
 			}
-		}
+		end
 	end
-	if GetVehicleLivery(vehicle) == -1 then
-		lib.notify({
-			id = 'no_livery',
-			title = 'Liveries',
-			description = 'No liveries found for this vehicle.',
-			position = 'top',
-			style = {
-				backgroundColor = '#141517',
-				color = '#909296'
-			},
-			icon = 'ban',
-			iconColor = '#C53030'
-		})
-	return end
+	
+	if numLiveries > 0 then
+		for i = 0, numLiveries -1 do
+			local modLabel = GetLiveryName(cache.vehicle, i)
+			options[#options + 1] = {
+				title = "Toggle livery " .. i,
+				description = GetLabelText(modLabel),
+				event = 'togglelivery',
+				args = {
+					vehicle = vehicle,
+					livery = i
+				}
+			}
+		end
+	end
 	
     lib.registerContext({
         id = 'liveries_menu',
@@ -58,5 +65,6 @@ end)
 
 AddEventHandler('togglelivery',function(data)
     SetVehicleLivery(data.vehicle,data.livery)
+	SetVehicleMod(data.vehicle,48,data.livery)
     TriggerEvent('getliveries')
 end)
